23c0dd374937d226847af612d10f072f286a66de
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2025-01-15 15:24:08 +0100

    fix: add missing timestamps

--- database/migrations/2025_01_15_122731_add_table__school_class.php
+++ database/migrations/2025_01_15_122731_add_table__school_class.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->foreignId('study_plan_id');
+            $table->timestamps();
         });
 
         Schema::table('users', function (Blueprint $table) {
