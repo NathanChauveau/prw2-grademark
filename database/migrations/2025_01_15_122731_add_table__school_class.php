7fb6320aaa2f6169302143b4b5f4b2023aefccc2
Nathan Chauveau <nathan.chauveau@eduvaud.ch>
2025-01-15 13:34:26 +0100

    feat: migration for SchooClass and 3 new columns

---
+++ database/migrations/2025_01_15_122731_add_table__school_class.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('school_class', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('study_plan_id')->nullable();
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('school_class_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('school_class');
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('school_class_id');
+            $table->dropColumn('school_class_id');
+        });
+    }
+};
